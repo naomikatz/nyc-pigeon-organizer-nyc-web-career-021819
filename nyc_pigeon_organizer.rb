35c2c44457b93200ab584d4b82806a13109c8244
Naomi Katz <naykatz@gmail.com>
2019-02-18 04:39:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+   bird_names = data[:gender][:male] + data[:gender][:female]
+   
+   new_hash = {}
+
+   bird_names.each do |name|
+    new_hash[name] = {}
+
+     data.each do |key, value|
+      new_hash[name][key] = []
+
+       value.each do |attribute, bird_names|
+        new_hash[name][key] << attribute.to_s if bird_names.include?(name)
+      end
+    end
+  end
+
+   return new_hash
 end
